0843e613b0b418e3c2cadf480e3b6dee0fba4a97
Mehedi Hasan <39464059+mehedifreelancer@users.noreply.github.com>
2019-10-01 10:32:11 +0600

    IBI2
    testing

--- js/myJs.js
+++ js/myJs.js
@@ -182,9 +182,10 @@
      })
  })
 
- //----------------------------------------------------Event slickcarusel--------------------------------------
- $('.singleEvent').slick({
-     infinite: true,
-     slidesToShow: 2,
-     slidesToScroll: 3
+
+ // Bootsrap dropdown on hover
+ $('ul.nav li.dropdown').hover(function() {
+     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+ }, function() {
+     $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
  });
